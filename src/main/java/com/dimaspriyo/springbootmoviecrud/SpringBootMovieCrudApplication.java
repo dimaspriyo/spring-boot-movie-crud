a705d1aec371ac64deb53aa6f421c066fee2044e
dimaspriyo <dimspriyo@gmail.com>
2020-04-29 14:39:26 +0700

    Starter Project

---
+++ src/main/java/com/dimaspriyo/springbootmoviecrud/SpringBootMovieCrudApplication.java
@@ -0,0 +1,13 @@
+package com.dimaspriyo.springbootmoviecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovieCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMovieCrudApplication.class, args);
+	}
+
+}
